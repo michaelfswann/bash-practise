4446c160a6beca3f35a8807410321c7521245193
michaelfswann <mikeswann@mail.com>
2021-01-23 22:23:19 +0000

    script updated following shell check

--- docker-build-and-run.sh
+++ docker-build-and-run.sh
@@ -18,10 +18,10 @@ while getopts 'a:b:c' flag; do
 done
 
 # build docker image
-docker build -t ${IMAGE_NAME} .
+docker build -t "${IMAGE_NAME}" .
 
-# run container with name and ports
-docker run --rm -it --name ${IMAGE_NAME} -p ${MAP_HOST_PORT}:${MAP_CONTAINER_PORT} $(docker images | awk '{print $3}' | awk 'NR==2')
+# run container with name and ports using most recently built image in docker images
+docker run --rm -it --name "${IMAGE_NAME}" -p "${MAP_HOST_PORT}":"${MAP_CONTAINER_PORT}" "$(docker images | awk '{print $3}' | awk 'NR==2')"
 
 # Below CMD runs container remapping to port 80
 # docker run --rm -it --name python-server -p 80:8080 $(docker images | awk '{print $1}' | awk 'NR==2')
