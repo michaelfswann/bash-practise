6b4a5ea975c15c6a2c9d7ca9578a5aaa46e428f1
michaelfswann <mikeswann@mail.com>
2021-01-24 22:37:47 +0000

    using case

--- learning.sh
+++ learning.sh
@@ -12,18 +12,53 @@
 
 # use read with -p (prompt)
 
-read -p "Enter your name: " NAME
+# read -p "Enter your name: " NAME
 
-echo Hello $NAME!
+# echo Hello $NAME!
 
-# simple if statement
+# # simple if statement
 
-if [ "$NAME" == "mike" ]
-then
-    echo "your name is mike"
-elif [ "$NAME" == "michael" ]
-then
-    echo "your name is michael"
-else
-    echo "your name is neither mike or michael"
-fi
+# if [ "$NAME" == "mike" ]
+# then
+#     echo "your name is mike"
+# elif [ "$NAME" == "michael" ]
+# then
+#     echo "your name is michael"
+# else
+#     echo "your name is neither mike or michael"
+# fi
+
+# TIME="$(date)"
+
+# echo $TIME
+# echo hi
+
+# -n skips new line for next command (eg echos on same line)
+
+echo -n "Enter the name of a country: "
+read COUNTRY
+
+echo -n "The official language of $COUNTRY is "
+
+case $COUNTRY in
+
+  UK)
+    echo "English"
+    ;;
+
+  Germany | Austria)
+    echo "German"
+    ;;
+
+  France | Belgium)
+    echo "French"
+    ;;
+
+  *)
+    echo "unknown"
+    ;;
+esac
+
+
+
+exit
